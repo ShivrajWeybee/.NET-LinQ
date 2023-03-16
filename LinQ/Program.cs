09583c8b74c5a7279ffcbe70b6f8b05df715eafb
Shivraj Chvada <shivraj@webtheunivesre.com>
2023-03-16 15:31:26 +0530

    solved

--- LinQ/Program.cs
+++ LinQ/Program.cs
@@ -13,25 +13,25 @@ namespace LinQ
             List<string> names1 = new List<string> { "Mark", "Dustiban", "Rancho", "Yastika", "Haranika", "Darsahn", "Yo-Yo", "Brandan", "Brandie", "Xerox", "Mark", "Rancho", "Darsahn", "X-men" };
 
             // get element from 5th index to next 5 element in list
-            //var result1 = names1.Skip(5).TakeWhile(x => x != names1.ElementAt(10));
+            var result1 = names1.Skip(5).TakeWhile(x => x != names1.ElementAt(10));
 
-            //foreach(var a in result1)
-            //{
-            //    Console.WriteLine(a);
-            //}
-            //Console.WriteLine();
+            foreach (var a in result1)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine();
 
             // get element which starts with "X"
-            IEnumerable<string> result2 = names1.TakeWhile(x => x.StartsWith("Y"));
+            IEnumerable<string> result2 = names1.Where(x => x.StartsWith("X"));
 
             foreach (string a in result2)
             {
                 Console.WriteLine(a);
             }
-            //Console.WriteLine();
+            Console.WriteLine();
 
             // get element which starts with "y"
-            var result3 = names1.TakeWhile(x => x.StartsWith("X"));
+            var result3 = names1.Where(x => x.StartsWith("Y"));
 
             foreach (string a in result3)
             {
