03bcb2b93acf979a32228abc6716639e10c300ff
Shivraj Chvada <shivraj@webtheunivesre.com>
2023-03-16 14:57:22 +0530

    Try joins

--- LinQ/Program.cs
+++ LinQ/Program.cs
@@ -10,17 +10,15 @@ namespace LinQ
     {
         static void Main(string[] args)
         {
-
-            //
             var empList = Employes.GetStudents();
             var deptList = Department.GetDepartments();
 
-            //
+            // ----------------------------------------------------------------------------------------------------------
+
             Department findDeptId;
-            
+
             try
             {
-                //
                 Console.WriteLine("Enter department name");
                 var deptName = Console.ReadLine();
 
@@ -29,14 +27,27 @@ namespace LinQ
 
                 foreach (var a in result)
                 {
-                    //Console.WriteLine($"Id : {a.Id}  Name : {a.Name}  Salary : {a.Salary}  DeptName : {deptList.Where(n => n.Id == a.DeptId).ToList().ElementAt(0).Name}");
-                    Console.WriteLine($"Id : {a.Id}  Name : {a.Name}  Salary : {a.Salary}  DeptName : {deptName}");
+                    Console.WriteLine($"Id : {a.Id}  Name : {a.Name}  Salary : {a.Salary}  DeptName : {deptList.Where(n => n.Id == a.DeptId).ToList().ElementAt(0).Name}");
+                    //Console.WriteLine($"Id : {a.Id}  Name : {a.Name}  Salary : {a.Salary}  DeptName : {deptName}");
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.StackTrace);
             }
+
+
+            // ----------------------------------------------------------------------------------------------------------
+
+
+            //var resultQS = from emp in empList
+            //               join dept in deptList
+            //               on emp.DeptId equals 
+            //               select new
+            //               {
+
+            //               };
+
 
             Console.ReadLine();
         }
